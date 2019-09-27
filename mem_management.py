33eb894fcf458a4dcc7efef0a210af03ab780657
Nehagarde <neha.r.garde@gmai.com>
2019-09-27 18:52:50 +0530

    Default Changelist

--- mem_management.py
+++ mem_management.py
@@ -2,18 +2,41 @@ import sys,copy
 import memory_profiler
 
 @profile
-def function():
-    x = list(range(1000000))  # allocate a big list
-    y = copy.deepcopy(x)
-    del x
-    return y
+def mem_management_for_integers():
+    a = 10
+    a = 2000
+    b = a
+    c = copy.deepcopy(a)
+    d = copy.copy(a)
 
 
 @profile
-def mem_management_for_integers():
-    a = 10
+def mem_management_for_strings():
+    a = "asdsad"
+    a = "dsffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff\
+        sadddasdsad10000000000000000000000ffffffffffffffffffffffffffffff"
+    b = a
+    c = copy.deepcopy(a)
+    d = copy.copy(a)
+    e = [a,c]
 
 
-if __name__ == '__main__':
-    function()
+@profile
+def mem_management_for_lists():
+    a = [1,2,3]
+    a = [1,2,3,1,a]
+    b = a
+    c = copy.deepcopy(a)
+    d = copy.copy(a)
+
+@profile
+def mem_management_for_dict():
+    a = {"asd":"1","dsafsf":"asds"}
+    a = [1, 2, 3, 1, a]
+    b = a
+    c = copy.deepcopy(a)
+    d = copy.copy(a)
+
+
+
     mem_management_for_integers()
